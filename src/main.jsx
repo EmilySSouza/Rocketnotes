4c21bfef45ba167c6f5ff5096afd42d7310bfb74
Emily Souza <emilyssouza05@gmail.com>
2025-02-09 16:17:50 -0300

    feat: Adding Global Styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { Details } from './pages/Details';
+import GlobalStyles from "./styles/global";
 import theme from "./styles/theme"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
   </StrictMode>
