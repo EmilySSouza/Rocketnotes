8402e6ef2fa34bd81c91963cd2765d2c023ec77a
Emily Souza <emilyssouza05@gmail.com>
2025-02-27 17:56:36 -0300

    feat: Add component Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,12 @@
-import { Container } from "./styles"
-import { Button } from "../../components/Button"
+import { Container } from "./styles";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
 
 export function Details() {
   
   return(
     <Container>
-      <h1>Hello World!</h1>
+      <Header />
       <Button title="Voltar"/>
     </Container>
   )
