7d953456393616d2db8b01b7f72de55e0da4bc8e
Emily Souza <emilyssouza05@gmail.com>
2025-03-06 03:15:51 -0300

    feat: add icon user image

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
     return (
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/EmilySSouza.png" alt="Foto do usuário" />
+
+                <div>
+                    <span>Bem-vinda</span>
+                    <strong>Emily Souza da Silva</strong>
+                </div>
+            </Profile>
         </Container>
     )
 }
