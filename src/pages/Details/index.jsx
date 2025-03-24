4c367d479cb8a3fbc4aed67d61ad1e20a700ab78
Emily Souza <emilyssouza05@gmail.com>
2025-03-24 07:44:15 -0300

    feat: add component tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   
@@ -12,9 +13,14 @@ export function Details() {
 
       <Section title="Links úteis"> 
         <Links>
-        <li><a href="#">https://www.rocketseat.com.br</a></li>
-        <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="Express"/>
+        <Tag title="Nodejs"/>
       </Section>
       
       <Button title="Voltar"/>
