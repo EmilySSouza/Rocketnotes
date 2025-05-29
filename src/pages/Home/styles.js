64df4fd338566f0c6e355bca404a5dd9dccdce48
Emily Souza <emilyssouza05@gmail.com>
2025-05-29 00:21:10 -0300

    feat: create input from home page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -51,10 +51,13 @@ export const Menu = styled.ul`
 
 export const Search = styled.div`
     grid-area: search;
+    padding: 64px 64px 0;
 `;
+
 export const Content = styled.div`
     grid-area: content
 `;
+
 export const NewNote = styled.button`
     grid-area: newnote;
 
